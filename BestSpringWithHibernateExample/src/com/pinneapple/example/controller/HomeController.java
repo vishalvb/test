b89c253c3cb653908309e954ac632c87f3b84f0f
pdalal <prerakdalal@gmail.com>
2014-11-22 16:10:05 -0500

    added log statements in controller class.

--- BestSpringWithHibernateExample/src/com/pinneapple/example/controller/HomeController.java
+++ BestSpringWithHibernateExample/src/com/pinneapple/example/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.pinneapple.example.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+	
+	private static final Logger log = Logger.getLogger(HomeController.class);
 	
 	@RequestMapping(value="/")
 	public ModelAndView mainPage() {
@@ -14,6 +17,7 @@ public class HomeController {
 
 	@RequestMapping(value="/index.pinneapple")
 	public ModelAndView indexPage() {
+		log.debug("inside  indexPage() method");
 		return new ModelAndView("home");
 	}
 
